768b533bf994564b004ddfcc1ec00e97f1140a22
Андрій <pentukandriy2083@gmail.com>
2024-07-22 20:50:52 +0300

    theme game plssssss

--- tgApp.js
+++ tgApp.js
@@ -4,11 +4,3 @@ const themeParams = tg.themeParams;
 tg.ready()
 
 tg.expand()
-
-document.addEventListener("DOMContentLoaded", function() {
-  if (themeParams.bg_color === '#ffffff') {
-    document.documentElement.style.setProperty('--tg-theme-header-bg-color', 'yellow');
-  } else if (themeParams.bg_color === '#000000') {
-    document.documentElement.style.setProperty('--tg-theme-header-bg-color', 'red');
-  }
-});
