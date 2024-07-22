ed5c1482baf31df916cf831f5dc894f3860d1990
Andess <115947439+Andrii5241@users.noreply.github.com>
2024-07-22 21:22:08 +0300

    Update tgApp.js

--- tgApp.js
+++ tgApp.js
@@ -4,3 +4,10 @@ const themeParams = tg.themeParams;
 tg.ready()
 
 tg.expand()
+
+if(tg.colorScheme = "dark"){
+  themeParams.text_color = '#F8EDED'
+}
+else if(tg.colorScheme = "light"){
+  themeParams.text_color = '#FF8225'
+}
