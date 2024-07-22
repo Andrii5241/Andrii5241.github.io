67a747fd120e79eb05142e7b7aad3e5b6c656b3d
Андрій <pentukandriy2083@gmail.com>
2024-07-22 20:34:33 +0300

    theme game

--- tgApp.js
+++ tgApp.js
@@ -1,5 +1,14 @@
 const tg = window.Telegram.WebApp;
+const themeParams = tg.themeParams;
 
 tg.ready()
 
 tg.expand()
+
+document.addEventListener("DOMContentLoaded", function() {
+  if (themeParams.bg_color === '#ffffff') {
+    document.documentElement.style.setProperty('--tg-theme-header-bg-color', 'yellow');
+  } else if (themeParams.bg_color === '#000000') {
+    document.documentElement.style.setProperty('--tg-theme-header-bg-color', 'red');
+  }
+});
